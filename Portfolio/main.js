dcfd1a1f87ccd0e4b82ae731fbe9906b76dc4627
Isha Sharma <94511920+ishaosharma@users.noreply.github.com>
2025-12-10 17:45:06 +0530

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,11 @@
+// ******************  TOGGLE ICON NAVBAR  ************************
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+
